f884b5c077bfd57d79d462ec8dce97a4ba4350e3
admin <admin@admins-MacBook-Pro-109.local>
2022-04-06 11:17:39 +0530

    trial2

--- Nav.js
+++ Nav.js
@@ -33,7 +33,7 @@ const MyStack = () => {
 function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen guyhua</Text>
+      <Text>Home Screen</Text>
       <Button
         title="Go to Details"
         onPress={() => navigation.navigate('Details')}
